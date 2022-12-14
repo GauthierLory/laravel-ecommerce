7f02c0187361de667d5fce7410da9bdb081b1e33
glory <gauthier.lory@protonmail.com>
2022-12-14 14:54:04 +0100

    change put increase/decrease

--- resources/js/composables/products/index.js
+++ resources/js/composables/products/index.js
@@ -23,11 +23,11 @@ export default function useProduct() {
     }
 
     const increaseQuantity =  async(id) => {
-      await axios.get('/api/products/increase/' + id)
+      await axios.put('/api/products/increase/' + id)
     }
 
     const decreaseQuantity =  async(id) => {
-        await axios.get('/api/products/decrease/' + id)
+        await axios.put('/api/products/decrease/' + id)
     }
     const destroyProduct =  async(id) => {
         await axios.delete('/api/products/' + id)
